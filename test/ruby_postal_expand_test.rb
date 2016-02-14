0e40f87b9654b1d7a73d55347103649df6907cee
Al <albarrentine@gmail.com>
2016-02-14 18:53:00 -0500

    [fix] Encoding declaration in Ruby tests

--- test/ruby_postal_expand_test.rb
+++ test/ruby_postal_expand_test.rb
@@ -1,3 +1,5 @@
+#!/bin/env ruby
+# encoding: utf-8
 require 'test_helper'
 require 'set'
 
--- test/ruby_postal_parser_test.rb
+++ test/ruby_postal_parser_test.rb
@@ -1,3 +1,6 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require 'test_helper'
 require 'ruby_postal/parser'
 
