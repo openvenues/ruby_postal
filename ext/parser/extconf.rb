c9c7307393f924d9480731eabef881bfee6a2c98
Al <albarrentine@gmail.com>
2016-02-14 18:39:47 -0500

    [build] Using dir_config in extconf.rb

--- ext/parser/extconf.rb
+++ ext/parser/extconf.rb
@@ -1,5 +1,39 @@
 require 'mkmf'
 
+LIBDIR      = Config::CONFIG['libdir']
+INCLUDEDIR  = Config::CONFIG['includedir']
+
+HEADER_DIRS = [
+  # First search /opt/local for macports
+  '/opt/local/include',
+
+  # Then search /usr/local for people that installed from source
+  '/usr/local/include',
+
+  # Check the ruby install locations
+  INCLUDEDIR,
+
+  # Finally fall back to /usr
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  # First search /opt/local for macports
+  '/opt/local/lib',
+
+  # Then search /usr/local for people that installed from source
+  '/usr/local/lib',
+
+  # Check the ruby install locations
+  LIBDIR,
+
+  # Finally fall back to /usr
+  '/usr/lib',
+]
+
+dir_config('libpostal', HEADER_DIRS, LIB_DIRS)
+
+$CFLAGS << " -std=gnu99"
 $LIBS << " -lpostal"
 
 create_makefile('cpostal/parser')
