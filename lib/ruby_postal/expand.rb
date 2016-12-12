d61ee9c0567019119c53ae80ad103727bf94d77b
Al <albarrentine@gmail.com>
2016-12-12 14:29:50 -0500

    [fix] using address or "" to handle nil, etc. Underlying C function will raise an exception if something like an array/hash is passed in

--- lib/ruby_postal/expand.rb
+++ lib/ruby_postal/expand.rb
@@ -3,7 +3,7 @@ require 'ruby_postal/cpostal/expand'
 module Postal
     class Expand
         def self.expand_address(address, options={})
-            CExpand.expand_address address, options
+            CExpand.expand_address address or "", options
         end
     end
 end
--- lib/ruby_postal/parser.rb
+++ lib/ruby_postal/parser.rb
@@ -3,7 +3,7 @@ require 'ruby_postal/cpostal/parser'
 module Postal
     class Parser
         def self.parse_address(address, options={})
-            parse_result = CParser.parse_address address.to_s, options
+            parse_result = CParser.parse_address address or "", options
             parse_result.map{|s, c| {:label => c, :value => s}}
         end
     end
